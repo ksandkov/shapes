d9d2f36cd9a34de99f57f964eede49ce1de4b9cf
Alexandre <ipatrool@gmail.com>
2017-05-26 17:19:45 +0300

    initial commit

--- shapes.py
+++ shapes.py
@@ -1,5 +1,4 @@
 import math
-import random
 
 
 class Shapes:
@@ -9,69 +8,27 @@ class Shapes:
 
     def __str__(self):
         return "Name: " + self.name
-
-
-#print(Shapes('NotSpam'))
-
-
-
-
+    
+    
 class Circle(Shapes):
     def __init__(self, name, radius: int):
         super().__init__(name)
         self.radius = radius
-    
+
 
     def get_area(self):
         area = self.radius * math.pi
-        print("Area of Circle is:", end = ' ')
+        print("Area of Circle is:", end=' ')
         return area
 
 
     def get_perimeter(self):
         perimeter = 2 * self.radius * math.pi
-        print("Perimeter of Circle is:", end = ' ')
+        print("Perimeter of Circle is:", end=' ')
         return perimeter
 
 
-shape = Circle("This is Circle", 25)
-print(shape.name)
-print(shape.get_area())
-print(shape.get_perimeter())
-
-
-
-
-
-
-
-class Rectangular(Shapes):
-    def __init__(self):
-       return "Name:{}; Shape1{}".format(
-            self.name,
-            self.shape
-            )
-    def get_area(self):
-        pass
-
-    def get_perimeter(self):
-        pass
-
-
-
-
-
-
-
-class Triangle(Shapes):
-    def __init__(self):
-       return "Name:{}; Shape1{}".format(
-            self.name,
-            self.shape
-            )
-    def get_area(self):
-        pass
-
-    def get_perimeter(self):
-        pass
-
+shape_1 = Circle("This is Circle", 25)
+print(shape_1.name)
+print(shape_1.get_area())
+print(shape_1.get_perimeter())
